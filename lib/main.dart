ad2be4a3f62d4ca9edb97a58065fab01497bd40e
ckellypk <ckelly@commure.com>
2022-01-16 22:01:16 -0500

    pushing finished challenge to github

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,56 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
-      ),
+void main() {
+  return runApp(
+    MaterialApp(
+      home:Scaffold(
+        backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: Text('Ask Me Anything'),
+            backgroundColor: Colors.blue.shade900,
+          ),
+          body: BallPage()),
+    ),
+
+  );
+}
+
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Ball();
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    int ballImage =Random().nextInt(5) + 1;
+    return Container(
+      child: Center(
+        child: Expanded(
+          child: TextButton(
+              child: Image.asset('images/ball$ballImage.png'),
+              onPressed: () {
+                setState(() {
+                  ballImage =  Random().nextInt(5) + 1;
+                });
+          },
+          ),
+        ),
+      )
     );
+  }
+}
+
+
+
+
